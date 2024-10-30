359196738223ab9914210e45864c1628127819ce
Braeden Barnett <bbarnett145@gmail.com>
2024-10-30 12:04:50 -0400

    Implemented "Reports" tab with Seller's Report functionality.

--- demo/src/main/java/com/example/demo/HelloApplication.java
+++ demo/src/main/java/com/example/demo/HelloApplication.java
@@ -4,19 +4,39 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Parent root = fxmlLoader.load();
-        Scene scene = new Scene(root, 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage stage) {
+        try {
+            // Load the main view
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+            Parent root = fxmlLoader.load();
+
+            // Create TabPane
+            TabPane tabPane = new TabPane();
+
+            // Create initial tab
+            Tab initialTab = new Tab("Home");
+            initialTab.setContent(root);
+
+            // Create and add reports tab
+            ReportsTab reportsTab = new ReportsTab();
+            tabPane.getTabs().addAll(initialTab, reportsTab.getReportsTab());
+
+            // Set up scene and stage
+            Scene scene = new Scene(tabPane, 320, 240);
+            stage.setTitle("Hello!");
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
